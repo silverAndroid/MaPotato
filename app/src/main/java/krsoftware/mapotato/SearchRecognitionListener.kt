0b33c6fafacae49e7cfccd51cedc2e00b5dd1b40
Kyle Kung <kkung@klipfolio.com>
2017-09-30 17:48:20 -0400

    Implemented speech to text conversion

---
+++ app/src/main/java/krsoftware/mapotato/SearchRecognitionListener.kt
@@ -0,0 +1,49 @@
+package krsoftware.mapotato
+
+import android.os.Bundle
+import android.speech.RecognitionListener
+import android.speech.SpeechRecognizer
+
+/**
+ * Created by kkung on 2017-09-30.
+ */
+
+class SearchRecognitionListener : RecognitionListener{
+    override fun onReadyForSpeech(p0: Bundle?) {
+        println("Ready For Speech")
+    }
+
+    override fun onRmsChanged(p0: Float) {
+        println("Rms Changed")
+    }
+
+    override fun onBufferReceived(p0: ByteArray?) {
+        println("Buffer Received")
+    }
+
+    override fun onPartialResults(p0: Bundle?) {
+        println("Partial Results")
+    }
+
+    override fun onEvent(p0: Int, p1: Bundle?) {
+        println("Event")
+    }
+
+    override fun onBeginningOfSpeech() {
+        println("Beginning of Speech")
+    }
+
+    override fun onEndOfSpeech() {
+        println("End of Speech")
+    }
+
+    override fun onError(p0: Int) {
+        println("Error" + p0)
+    }
+
+    override fun onResults(results: Bundle?) {
+        var arrayResults: ArrayList<String>? = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)
+        println(arrayResults)
+    }
+
+}
