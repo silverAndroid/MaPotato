ab37732f525f12abe494a6e6a79950a988a96a84
Rushil Perera <pererarushil1940@gmail.com>
2017-10-01 13:25:17 -0400

    Added extension function to inflate layouts easier in Adapters

--- app/src/main/java/krsoftware/mapotato/extensions.kt
+++ app/src/main/java/krsoftware/mapotato/extensions.kt
@@ -1,11 +1,17 @@
 package krsoftware.mapotato
 
+import android.support.annotation.LayoutRes
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 
 /**
  * Created by silver_android on 30/09/17.
  */
+
+fun ViewGroup.inflate(@LayoutRes resource: Int, attachToRoot: Boolean = false): View = LayoutInflater.from(context).inflate(resource, this, attachToRoot)
 
 fun AppCompatActivity.showFragment(fragment: Fragment, addToStack: Boolean = false, name: String? = "") {
     val transaction = supportFragmentManager.beginTransaction()
