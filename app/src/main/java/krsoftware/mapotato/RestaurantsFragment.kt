fe9470e25f5e93c4cea318edf4edca48a4cf0271
Kyle Kung <kkung@klipfolio.com>
2017-10-01 15:44:31 -0400

    request location permission and get the last known location to do the map search on

--- app/src/main/java/krsoftware/mapotato/RestaurantsFragment.kt
+++ app/src/main/java/krsoftware/mapotato/RestaurantsFragment.kt
@@ -1,8 +1,12 @@
 package krsoftware.mapotato
 
 
+import android.Manifest
 import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
+import android.annotation.SuppressLint
+import android.content.Context
+import android.location.Location
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
@@ -11,24 +15,37 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import kotlinx.android.synthetic.main.fragment_restaurants.*
 import krsoftware.mapotato.model.Place
 import krsoftware.mapotato.retrofit.MapsAPIService
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.RuntimePermissions
+import com.google.android.gms.tasks.OnSuccessListener
+
+
 
 /**
  * A simple [Fragment] subclass.
  */
+@RuntimePermissions
 class RestaurantsFragment : Fragment() {
     private val mapsAPI: MapsAPIService = MapsAPIService()
     private lateinit var query: String
     private lateinit var adapter: RestaurantsAdapter
+    private lateinit var mFusedLocationProviderClient: FusedLocationProviderClient
+    private var mLocation: Location = Location(Context.LOCATION_SERVICE)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         query = arguments.getString(ARG_QUERY)
+        mLocation.latitude = 45.4276550
+        mLocation.longitude = -75.6824510
+        getLocationWithPermissionCheck()
     }
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -39,7 +56,7 @@ class RestaurantsFragment : Fragment() {
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        mapsAPI.search(query, 5000, "45.4276550,-75.6824510")
+        mapsAPI.search(query, 5000, mLocation.toString())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.io())
                 .subscribe({ response ->
@@ -91,6 +108,20 @@ class RestaurantsFragment : Fragment() {
                 .setListener(object : AnimatorListenerAdapter() {
                     override fun onAnimationEnd(animation: Animator) {
                         hideView.visibility = View.GONE
+                    }
+                })
+    }
+
+    @SuppressLint("MissingPermission")
+    @NeedsPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    fun getLocation() {
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this.activity)
+        mFusedLocationProviderClient.getLastLocation()
+                .addOnSuccessListener(this.activity, { location ->
+                    if (location != null) {
+                        mLocation = location
+                    } else {
+                        //no location no search is done, will handle this case in the future.
                     }
                 })
     }
--- app/src/main/java/krsoftware/mapotato/retrofit/MapsAPIService.kt
+++ app/src/main/java/krsoftware/mapotato/retrofit/MapsAPIService.kt
@@ -25,10 +25,10 @@ class MapsAPIService {
         api = retrofit.create(MapsAPI::class.java)
     }
 
-    fun search(query: String, radius: Int, location: String = "12.45,31.95") : Single<SearchResults> = api.search(query, location, radius)
+    fun search(query: String, radius: Int, location: String) : Single<SearchResults> = api.search(query, location, radius)
 
     interface MapsAPI {
         @GET("/maps/api/place/nearbysearch/json")
-        fun search(@Query("keyword") query: String, @Query("location") location: String = "123.45,231.45", @Query("radius") radius: Int, @Query("key") key: String = BuildConfig.API_KEY) : Single<SearchResults>
+        fun search(@Query("keyword") query: String, @Query("location") location: String, @Query("radius") radius: Int, @Query("key") key: String = BuildConfig.API_KEY) : Single<SearchResults>
     }
 }
