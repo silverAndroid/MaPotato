8666b1a3a0e2bc0df219edac9962c56bd960d8b8
Rushil Perera <pererarushil1940@gmail.com>
2017-10-01 13:23:33 -0400

    Shows the restaurant fragment

--- app/src/main/java/krsoftware/mapotato/MainActivity.kt
+++ app/src/main/java/krsoftware/mapotato/MainActivity.kt
@@ -2,11 +2,19 @@ package krsoftware.mapotato
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.facebook.drawee.backends.pipeline.Fresco
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Fresco.initialize(this)
+        showRestaurantsFragment("Google")
+    }
+
+    private fun showRestaurantsFragment(query: String) {
+        showFragment(RestaurantsFragment.newInstance(query), true, "RestaurantsFragment")
     }
 }
