d71327009b5d89f4bf0c9cb38b06277b4502e9ff
Rushil Perera <pererarushil1940@gmail.com>
2017-10-01 16:20:18 -0400

    Added next_page_token for infinite scrolling

--- app/src/main/java/krsoftware/mapotato/model/SearchDTO.kt
+++ app/src/main/java/krsoftware/mapotato/model/SearchDTO.kt
@@ -3,7 +3,7 @@ package krsoftware.mapotato.model
 /**
  * Created by silver_android on 30/09/17.
  */
-data class SearchResults(val results: List<PlaceDTO>, val status: String, val error_message: String?)
+data class SearchResults(val results: List<PlaceDTO>, val status: String, val error_message: String?, val next_page_token: String?)
 
 data class PlaceDTO(val photos: List<PhotoDTO>?, val rating: Double, val name: String, val place_id: String, val vicinity: String) {
     constructor(place: Place) : this(place.photos, place.rating, place.name, place.placeID, place.address)
