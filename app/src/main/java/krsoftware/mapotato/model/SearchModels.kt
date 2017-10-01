55b5b4f10a030fe00e14cf1c0b05c2e173496acb
Rushil Perera <pererarushil1940@gmail.com>
2017-10-01 15:43:51 -0400

    Photos may be null now

--- app/src/main/java/krsoftware/mapotato/model/SearchModels.kt
+++ app/src/main/java/krsoftware/mapotato/model/SearchModels.kt
@@ -5,7 +5,7 @@ package krsoftware.mapotato.model
  */
 class PlacesPhoto
 
-data class Place(val photos: List<PhotoDTO>, val rating: Double, val name: String, val placeID: String, val address: String) {
+data class Place(val photos: List<PhotoDTO>?, val rating: Double, val name: String, val placeID: String, val address: String) {
     constructor(dto: PlaceDTO) : this(dto.photos, dto.rating, dto.name, dto.place_id, dto.vicinity)
 
     fun toDTO(): PlaceDTO = PlaceDTO(photos, rating, name, placeID, address)
